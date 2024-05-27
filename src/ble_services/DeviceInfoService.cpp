f6165b013f01f79530c723f40a2b177d9a8517b0
Anshul Gupta <ansg191@anshulg.com>
2024-05-26 21:17:02 -0700

    Revert 81a0d59 & b074bb1

--- src/ble_services/DeviceInfoService.cpp
+++ src/ble_services/DeviceInfoService.cpp
@@ -6,10 +6,10 @@
 
 namespace ble_services {
 
-//BLEUUID DeviceInfoService::SERVICE_UUID = BLEUUID((uint16_t) 0x180A);
-BLEUUID DeviceInfoService::SERVICE_UUID = BLEUUID("926c28b7-78e4-4702-a61f-6d59a3d92ef5");
-//BLEUUID DeviceInfoService::NAME_UUID = BLEUUID((uint16_t) 0x2A00);
-BLEUUID DeviceInfoService::NAME_UUID = BLEUUID("a39f17f5-04d1-4b92-9463-f929207076b4");
+BLEUUID DeviceInfoService::SERVICE_UUID = BLEUUID((uint16_t) 0x180A);
+//BLEUUID DeviceInfoService::SERVICE_UUID = BLEUUID("926c28b7-78e4-4702-a61f-6d59a3d92ef5");
+BLEUUID DeviceInfoService::NAME_UUID = BLEUUID((uint16_t) 0x2A00);
+//BLEUUID DeviceInfoService::NAME_UUID = BLEUUID("a39f17f5-04d1-4b92-9463-f929207076b4");
 BLEUUID DeviceInfoService::VERSION_UUID = BLEUUID((uint16_t) 0x2A28);
 
 const char *NAME = "ESP32 Fridge";
--- src/ble_services/TimeService.cpp
+++ src/ble_services/TimeService.cpp
@@ -12,10 +12,10 @@ static ESP32Time rtc;
 
 uint64_t from_bytes(const uint8_t *data, size_t len);
 
-//BLEUUID TimeService::SERVICE_UUID = BLEUUID((uint16_t) 0x1847);
-BLEUUID TimeService::SERVICE_UUID = BLEUUID("db7b23f7-9cb9-4411-b7c0-73105fe56919");
-//BLEUUID TimeService::GET_TIME_UUID = BLEUUID((uint16_t) 0x2A2B);
-BLEUUID TimeService::GET_TIME_UUID = BLEUUID("8b7bfc03-2a3e-44d7-833a-17917eb180f2");
+BLEUUID TimeService::SERVICE_UUID = BLEUUID((uint16_t) 0x1847);
+//BLEUUID TimeService::SERVICE_UUID = BLEUUID("db7b23f7-9cb9-4411-b7c0-73105fe56919");
+BLEUUID TimeService::GET_TIME_UUID = BLEUUID((uint16_t) 0x2A2B);
+//BLEUUID TimeService::GET_TIME_UUID = BLEUUID("8b7bfc03-2a3e-44d7-833a-17917eb180f2");
 BLEUUID TimeService::SET_TIME_UUID = BLEUUID("65625049-d533-4c6c-9c2b-b2a1291d146f");
 
 class GetTimeCallbacks : public BLECharacteristicCallbacks {
